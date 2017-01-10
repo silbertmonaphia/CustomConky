0d2ae00ee6874bfd38da592c90717dc7fa4ca3cd
silbertmonaphia <silbertmonaphia@gmail.com>
2017-01-10 23:54:14 +0800

    加入正则匹配实时温度

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # encoding:utf-8
 
-import requests,json
+import requests,json,re
 from pprint import pprint
 
 with open('/etc/conky/config.json') as f:
@@ -19,9 +19,15 @@ t=json.loads(r.text)
 weather_data=t['results'][0]['weather_data']
 
 tody=weather_data[0]
+pattern = re.compile(r'\(.*\)')
+rt=pattern.search(tody['date'])
+
 tomm=weather_data[1]
 tdat=weather_data[2]
 tdft=weather_data[3]
 
-print ('今天',tody['temperature'],'  明天',tomm['temperature'])
-print ('后天',tdat['temperature'],'  大后天',tdft['temperature'])
+
+print ('今天',tody['temperature'],tody['weather'],rt.group(0))
+print (tomm['date'],tomm['temperature'],tomm['weather'])
+print (tdat['date'],tdat['temperature'],tdat['weather'])
+print (tdft['date'],tdft['temperature'],tdft['weather'])
